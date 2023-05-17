b6eabd64f3b18a393824ee42f1a9203dd3bf44d3
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-05-17 16:01:02 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "demo-layouts-40763",
-  url: "https://demo-layouts-40763.botics.co",
-  api: "https://demo-layouts-40763.botics.co/api/v1"
-}
+  "name": "demo-layouts-40763",
+  "url": "https://demo-layouts-40763.botics.co",
+  "api": "https://demo-layouts-40763.botics.co/api/v1",
+  "initialRoute": "Untitled17"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};

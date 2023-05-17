61673e8ce9c37b3e3993eafb2e7156cd4f54abbc
Shoaib Amjad <muhammad.shoaib@crowdbotics.com>
2023-05-17 16:36:25 +0500

    fixed gradle issues

--- screens/Untitled17/index.js
+++ screens/Untitled17/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Pressable } from "react-native";
 import { Text } from "react-native";
@@ -6,198 +7,115 @@ import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
 const Untitled17 = () => {
   const navigation = useNavigation();
   return <SafeAreaView style={styles.safeArea}>
-      <ScrollView contentContainerStyle={{
-      backgroundColor: "#f0f0f1",
-      padding: 10,
-      position: "relative",
-      flex: 1
-    }}>
-        <Pressable onPress={() => {
+    <ScrollView >
+      <Pressable onPress={() => {
         navigation.navigate("fiveRows");
       }}>
-          <View style={styles.GrUxYZPR}>
-            <Text style={styles.qvUZqSfd}>{"Five Rows"}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => {
+        <View style={styles.container}>
+          <Text style={styles.text}>{"Five Rows"}</Text>
+        </View>
+      </Pressable>
+      <Pressable onPress={() => {
         navigation.navigate("fourColumns");
       }}>
-          <View style={styles.NDSNeIDe}>
-            <Text style={styles.VEmjizrw}>{"Four Columns"}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => {
+        <View style={styles.container}>
+          <Text style={styles.text}>{"Four Columns"}</Text>
+        </View>
+      </Pressable>
+      <Pressable onPress={() => {
         navigation.navigate("fourRows");
       }}>
-          <View style={styles.BWdDlMul}>
-            <Text style={styles.txNgjuXz}>{"Four Rows"}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => {
+        <View style={styles.container}>
+          <Text style={styles.text}>{"Four Rows"}</Text>
+        </View>
+      </Pressable>
+      <Pressable onPress={() => {
         navigation.navigate("largeGrid");
       }}>
-          <View style={styles.NrwHabIZ}>
-            <Text style={styles.KMsSrZiH}>{"Large Grid"}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => {
+        <View style={styles.container}>
+          <Text style={styles.text}>{"Large Grid"}</Text>
+        </View>
+      </Pressable>
+      <Pressable onPress={() => {
         navigation.navigate("largeGrid1");
       }}>
-          <View style={styles.XhPJgYHt}>
-            <Text style={styles.shBGsztB}>{"Extra Large Grid"}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => {
+        <View style={styles.container}>
+          <Text style={styles.text}>{"Extra Large Grid"}</Text>
+        </View>
+      </Pressable>
+      <Pressable onPress={() => {
         navigation.navigate("manyRows");
       }}>
-          <View style={styles.PtktdzOD}>
-            <Text style={styles.OJBDlJBZ}>{"Many Rows"}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => {
+        <View style={styles.container}>
+          <Text style={styles.text}>{"Many Rows"}</Text>
+        </View>
+      </Pressable>
+      <Pressable onPress={() => {
         navigation.navigate("mosaic1");
       }}>
-          <View style={styles.JtIYEdEW}>
-            <Pressable>
-              <Text style={styles.WABeQrrS}>{"Single Column Grid"}</Text>
-            </Pressable>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => {
+        <View style={styles.container}>
+        <Text style={styles.text}>{"Single Column Grid"}</Text>
+        </View>
+      </Pressable>
+      <Pressable onPress={() => {
         navigation.navigate("mosaic2");
       }}>
-          <View style={styles.SdqRDqWp}>
-            <Text style={styles.DBsMKRRO}>{"Two Column Grid"}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => {
+        <View style={styles.container}>
+          <Text style={styles.text}>{"Two Column Grid"}</Text>
+        </View>
+      </Pressable>
+      <Pressable onPress={() => {
         navigation.navigate("smallGrid");
       }}>
-          <View style={styles.CMedVtGB}>
-            <Text style={styles.qnzYwhjt}>{"Flex Gird"}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => {
+        <View style={styles.container}>
+          <Text style={styles.text}>{"Flex Gird"}</Text>
+        </View>
+      </Pressable>
+      <Pressable onPress={() => {
         navigation.navigate("mosaic4");
       }}>
-          <View style={styles.CMedVtGB}>
-            <Text style={styles.qnzYwhjt}>{"Grid (3 * 1)"}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => {
+        <View style={styles.container}>
+          <Text style={styles.text}>{"Grid (3 * 1)"}</Text>
+        </View>
+      </Pressable>
+      <Pressable onPress={() => {
         navigation.navigate("mosaic5");
       }}>
-          <View style={styles.CMedVtGB}>
-            <Text style={styles.qnzYwhjt}>{"Grid (3 * 2)"}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => {
+        <View style={styles.container}>
+          <Text style={styles.text}>{"Grid (3 * 2)"}</Text>
+        </View>
+      </Pressable>
+      <Pressable onPress={() => {
         navigation.navigate("mosaic6");
-      }}><View style={styles.CMedVtGB}>
-          <Text style={styles.qnzYwhjt}>{"Stretch Grid"}</Text>
+      }}><View style={styles.container}>
+          <Text style={styles.text}>{"Stretch Grid"}</Text>
         </View></Pressable>
-        <Pressable onPress={() => {
+      <Pressable onPress={() => {
         navigation.navigate("smallGrid");
-      }}><View style={styles.CMedVtGB}>
-          <Text style={styles.qnzYwhjt}>{"Small Grid (8 * 4)"}</Text>
+      }}><View style={styles.container}>
+          <Text style={styles.text}>{"Small Grid (8 * 4)"}</Text>
         </View></Pressable>
-      </ScrollView>
-    </SafeAreaView>;
+    </ScrollView>
+  </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: "100%"
+    height: "100%",
+    padding: 10
   },
-  GrUxYZPR: {
+  container: {
     height: 60,
     width: "100%",
-    backgroundColor: "#E4E4E4",
+    backgroundColor: "lightblue",
     display: "flex",
-    justifyContent: "center"
+    justifyContent: "center",
+    marginBottom: 10,
+    borderRadius: 2
   },
-  qvUZqSfd: {
-    textAlign: "center"
-  },
-  NDSNeIDe: {
-    height: 60,
-    width: "100%",
-    backgroundColor: "#E4E4E4",
-    display: "flex",
-    justifyContent: "center"
-  },
-  VEmjizrw: {
-    textAlign: "center"
-  },
-  BWdDlMul: {
-    height: 60,
-    width: "100%",
-    backgroundColor: "#E4E4E4",
-    display: "flex",
-    justifyContent: "center"
-  },
-  txNgjuXz: {
-    textAlign: "center"
-  },
-  NrwHabIZ: {
-    height: 60,
-    width: "100%",
-    backgroundColor: "#E4E4E4",
-    display: "flex",
-    justifyContent: "center"
-  },
-  KMsSrZiH: {
-    textAlign: "center"
-  },
-  XhPJgYHt: {
-    height: 60,
-    width: "100%",
-    backgroundColor: "#E4E4E4",
-    display: "flex",
-    justifyContent: "center"
-  },
-  shBGsztB: {
-    textAlign: "center"
-  },
-  PtktdzOD: {
-    height: 60,
-    width: "100%",
-    backgroundColor: "#E4E4E4",
-    display: "flex",
-    justifyContent: "center"
-  },
-  OJBDlJBZ: {
-    textAlign: "center"
-  },
-  JtIYEdEW: {
-    height: 78,
-    width: 328,
-    backgroundColor: "#E4E4E4",
-    display: "flex",
-    justifyContent: "center"
-  },
-  WABeQrrS: {
-    textAlign: "center"
-  },
-  SdqRDqWp: {
-    height: 60,
-    width: "100%",
-    backgroundColor: "#E4E4E4",
-    display: "flex",
-    justifyContent: "center"
-  },
-  DBsMKRRO: {
-    textAlign: "center"
-  },
-  CMedVtGB: {
-    height: 60,
-    width: "100%",
-    backgroundColor: "#E4E4E4",
-    display: "flex",
-    justifyContent: "center"
-  },
-  qnzYwhjt: {
-    textAlign: "center"
+  text: {
+    textAlign: "left",
+    padding: 10
   }
 });
 export default Untitled17;
